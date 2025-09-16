4b54c500153a427e374ba67700abc8ad9b5cdffb
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-16 13:03:11 +0100

    Add some test code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono"
+import { Geohashing } from "./geohashing"
+import { DateTime } from "luxon"
 
 const app = new Hono()
+console.log("Starting Geohash Alert")
 
 app.get("/api/v1/hello", (c) => {
   return c.text("Hello Hono!")
 })
+
+const geohashing = new Geohashing()
+const geoHash = await geohashing.getGeohash(DateTime.now(), ["51", "-0"])
+console.log("GeoHash:", geoHash)
 
 export default app
