dfac66bb1ea0304137af2d005acc7a0a7c28d26b
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-16 23:39:27 +0100

    we have a form with Validation!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,49 @@ import { $ } from "voby"
 function App(): JSX.Element {
   return (
     <div>
-      <header>
-        <h1>Geohash Alert</h1>
-      </header>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-        }}
-      >
-        <label for="home-coords">Enter your home coordinates:</label>
-        <input type="text" id="home-coords" placeholder="51.50741, 0.12782" />
-        <button type="submit">Submit</button>
-      </form>
+      <div class="navbar bg-base-100 shadow-sm">
+        <a class="btn btn-ghost text-xl" href="/">
+          Geohash Alert
+        </a>
+      </div>
+      <main class="py-6 px-4">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            const input = document.getElementById("home-coords")
+            if (!(input instanceof HTMLInputElement))
+              throw new Error("Failed to find input element")
+            const coords = input.value.split(",").map((x) => x.trim())
+            if (coords.length !== 2 || coords.some((x) => isNaN(Number(x)))) {
+              input.setCustomValidity(
+                "Please enter valid coordinates (in the format 51.50741, 0.12782)"
+              )
+              input.reportValidity()
+              return
+            }
+            input.setCustomValidity("")
+            alert("yay")
+          }}
+        >
+          <label class="floating-label">
+            <span>Enter your home coordinates</span>
+            <input
+              type="text"
+              placeholder="Enter your home coordinates, e.g. 51.50741, 0.12782"
+              id="home-coords"
+              required
+              class="input input-md w-full max-w-sm"
+              onInput={(e) => {
+                const input = e.target as HTMLInputElement
+                input.setCustomValidity("")
+              }}
+            />
+          </label>
+          <button type="submit" class="btn mt-6">
+            Subscribe to alerts
+          </button>
+        </form>
+      </main>
     </div>
   )
 }
