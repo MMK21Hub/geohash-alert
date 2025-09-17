1b34383a9f0f79ccab9e417b5c1310be45b143d0
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-17 18:32:36 +0100

    uhhh so it no longer works in Firefox

--- frontend/src/service-worker-manager.ts
+++ frontend/src/service-worker-manager.ts
@@ -1,9 +1,15 @@
-export const serviceWorkerRegistration =
-  navigator.serviceWorker.register("/sw.js")
+export let serviceWorkerRegistration = navigator.serviceWorker.register(
+  "/sw.js",
+  { type: "module" }
+)
 
 serviceWorkerRegistration.then(async (reg) => {
-  await reg.update()
-  console.debug("Updated service worker registration", reg)
+  await reg.unregister()
+  serviceWorkerRegistration = navigator.serviceWorker.register("/sw.js", {
+    type: "module",
+  })
+  await serviceWorkerRegistration
+  console.debug("Refreshed service worker", reg)
 })
 
 export {}
