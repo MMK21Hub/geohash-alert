a82607abd25f0d02d2aa7b4d1d8ad77bd526470a
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-17 14:58:59 +0100

    Better coord formatting

--- frontend/sw.ts
+++ frontend/sw.ts
@@ -1,9 +1,14 @@
+import type { LatLng } from "@mmk21/geohashing"
 import type { PushMessage } from "../backend/src/message"
 
 /// <reference lib="webworker" />
 self.addEventListener("push", (event: PushEvent) =>
   event.waitUntil(handlePush(event))
 )
+
+function formatCoords(coords: LatLng): string {
+  return `${coords[0].toFixed(6)}, ${coords[1].toFixed(6)}`
+}
 
 async function handlePush(event: PushEvent) {
   const data: PushMessage = event.data?.json()
@@ -12,7 +17,9 @@ async function handlePush(event: PushEvent) {
   const title = data.isTest
     ? "Geohash Alert (manually triggered)"
     : "Nearby geohash"
-  const body = `Today's geohash for ${data.geohash.graticule} is TODOm away, at ${data.geohash.location}`
+  const body = `Today's geohash for ${
+    data.geohash.graticule
+  } is TODOm away, at ${formatCoords(data.geohash.location)}`
   await selfRegistration.showNotification(title, {
     body: body,
     icon: "/icon.png",
