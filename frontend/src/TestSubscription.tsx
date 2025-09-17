e38a18d43f1c105d93e94c8a525aca42d723130a
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-17 22:41:30 +0100

    Update TestSubscription layout to use daisyUI

--- frontend/src/TestSubscription.tsx
+++ frontend/src/TestSubscription.tsx
@@ -4,20 +4,24 @@ export default function TestSubscription({
   subscription: PushSubscription
 }) {
   return (
-    <div class="p-4 mt-12 bg-base-200 rounded-md shadow-md w-full max-w-sm space-y-4">
-      <h2 class="text-xl font-semibold ">Test your subscription</h2>
-      <button
-        class="btn btn-neutral"
-        onClick={() => {
-          fetch("/api/v1/test-subscription", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ subscription }),
-          })
-        }}
-      >
-        Send notification
-      </button>
+    <div class="card bg-base-100 shadow-md mt-8 mb-4">
+      <div class="card-body p-4">
+        <h2 class="card-title text-lg font-semibold mb-2">
+          Test your subscription
+        </h2>
+        <button
+          class="btn btn-neutral"
+          onClick={() => {
+            fetch("/api/v1/test-subscription", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ subscription }),
+            })
+          }}
+        >
+          Send notification
+        </button>
+      </div>
     </div>
   )
 }
