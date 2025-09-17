4c45e6742ede912428f647ed16a02ea7127fcd6b
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-17 11:38:25 +0100

    Finish writing subscribeToAlerts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,32 @@
 import { $ } from "voby"
 import { serviceWorkerRegistration } from "./service-worker-manager"
+import { Geohashing, Graticule, LatLng } from "@mmk21/geohashing"
 
-type Graticule = [string, string]
-
-async function subscribeToAlerts(graticule: Graticule) {
-  await serviceWorkerRegistration
-  // TODO...
+async function subscribeToAlerts(homeCoords: LatLng) {
+  const sw = await serviceWorkerRegistration
+  const homeGraticule = Geohashing.coordsToGraticule(...homeCoords)
+  const subscription = await sw.pushManager.subscribe({
+    userVisibleOnly: true,
+    // TODO: env
+    applicationServerKey:
+      "BOaKJCR8niTjyJm8Bcuh4GZKdmGLd4rOgH5a63xYmDWAvSRKK4F4ednFKfm92Qnm6vXmDJMsdr-tfGM7thl2EBU",
+  })
+  const res = await fetch("/api/v1/subscribe", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      subscription,
+      homeGraticule,
+    }),
+  })
+  const data = await res.json()
+  if (!res.ok) {
+    console.error("Failed to subscribe, API request failed", data)
+    return alert(`Failed to subscribe: ${data.error.message}`)
+  }
+  alert("Subscribed :D")
 }
 
 function App(): JSX.Element {
