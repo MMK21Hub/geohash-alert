2fc2d6c9b9ea1dec41ae9eceaad55b589b0d9652
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-09-17 14:18:35 +0100

    Make test alerts clearer

--- frontend/sw.ts
+++ frontend/sw.ts
@@ -8,14 +8,17 @@ async function handlePush(event: PushEvent) {
   const selfRegistration = await navigator.serviceWorker.getRegistration("/")
   console.log("Registration", selfRegistration, "sending notification", data)
   if (data.type === "test-alert") {
-    await selfRegistration?.showNotification("Geohash Alert", {
-      body: `This is a test alert sent at ${data.time}`,
-      icon: "/icon.png",
-    })
+    await selfRegistration?.showNotification(
+      "Geohash Alert (manually triggered)",
+      {
+        body: `This is a test alert sent at ${data.time}`,
+        icon: "/icon.png",
+      }
+    )
     return
   }
   await selfRegistration.showNotification(data.title || "Geohash Alert", {
-    body: data.body || "",
+    body: data.body || JSON.stringify(data),
     icon: "/icon.png",
   })
 }
